a4526b4765c0c659669cf7d730df7c0fe449462b
Karthik Nayak <Karthik.188@gmail.com>
2017-02-04 17:41:24 +0530

    replace pointers with arrays
    Instead of having pointers and linked lists, which overly complicates
    everything. Just use arrays for each face.

--- main.go
+++ main.go
@@ -15,32 +15,33 @@ const (
 	cyan    = 5
 )
 
-type sq struct {
-	right, top *sq
-	color      int
-}
-
-func (s *sq) r(x int) *sq {
-	var tmp *sq = s
-	for i := 0; i < x; i++ {
-		tmp = tmp.right
-	}
-	return tmp
-}
-
-func (s *sq) t(x int) *sq {
-	var tmp *sq = s
-	for i := 0; i < x; i++ {
-		tmp = tmp.top
-	}
-	return tmp
-}
-
 type cube struct {
-	r1, r2, r3, c1, c2, c3 *sq
+	f [3][3]int
+	l [3][3]int
+	r [3][3]int
+	u [3][3]int
+	d [3][3]int
+	b [3][3]int
 }
 
-func colorIt() func(clr int) string {
+func initSide(s *[3][3]int, clr int) {
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			s[i][j] = clr
+		}
+	}
+}
+
+func (c *cube) init() {
+	initSide(&c.f, red)
+	initSide(&c.l, green)
+	initSide(&c.r, yellow)
+	initSide(&c.u, blue)
+	initSide(&c.b, magenta)
+	initSide(&c.d, cyan)
+}
+
+func colorSq() func(clr int) string {
 	red := color.New(color.FgRed).SprintFunc()
 	green := color.New(color.FgGreen).SprintFunc()
 	yellow := color.New(color.FgYellow).SprintFunc()
@@ -68,129 +69,33 @@ func colorIt() func(clr int) string {
 }
 
 func (c *cube) printCube() {
-	clr := colorIt()
+	clr := colorSq()
 
 	fmt.Println("        ---------")
-	for i := 3; i >= 1; i-- {
-		fmt.Printf("        | %s %s %s |\n", clr(c.c1.t(i).color),
-			clr(c.c2.t(i).color), clr(c.c3.t(i).color))
+	for i := 0; i < 3; i++ {
+		fmt.Printf("        | %s %s %s |\n", clr(c.u[i][0]),
+			clr(c.u[i][1]), clr(c.u[i][2]))
 	}
-
 	fmt.Println("---------------------------------")
 
-	fmt.Printf("| %s %s %s |", clr(c.r1.r(9).color), clr(c.r1.r(10).color), clr(c.r1.r(11).color))
-	for i := 0; i <= 6; i = i + 3 {
-		fmt.Printf(" %s %s %s |", clr(c.r1.r(i).color), clr(c.r1.r(i+1).color), clr(c.r1.r(i+2).color))
+	for i := 0; i < 3; i++ {
+		fmt.Printf("| %s %s %s |", clr(c.l[i][0]), clr(c.l[i][1]), clr(c.l[i][2]))
+		fmt.Printf(" %s %s %s |", clr(c.f[i][0]), clr(c.f[i][1]), clr(c.f[i][2]))
+		fmt.Printf(" %s %s %s |", clr(c.r[i][0]), clr(c.r[i][1]), clr(c.r[i][2]))
+		fmt.Printf(" %s %s %s |", clr(c.b[i][0]), clr(c.b[i][1]), clr(c.b[i][2]))
+		fmt.Println()
 	}
-	fmt.Printf("\n")
-
-	fmt.Printf("| %s %s %s |", clr(c.r2.r(9).color), clr(c.r2.r(10).color), clr(c.r2.r(11).color))
-	for i := 0; i <= 6; i = i + 3 {
-		fmt.Printf(" %s %s %s |", clr(c.r2.r(i).color), clr(c.r2.r(i+1).color), clr(c.r2.r(i+2).color))
-	}
-	fmt.Printf("\n")
-
-	fmt.Printf("| %s %s %s |", clr(c.r3.r(9).color), clr(c.r3.r(10).color), clr(c.r3.r(11).color))
-	for i := 0; i <= 6; i = i + 3 {
-		fmt.Printf(" %s %s %s |", clr(c.r3.r(i).color), clr(c.r3.r(i+1).color), clr(c.r3.r(i+2).color))
-	}
-	fmt.Printf("\n")
-
 	fmt.Println("---------------------------------")
 
-	for i := 9; i >= 7; i-- {
-		fmt.Printf("        | %s %s %s |\n", clr(c.c1.t(i).color),
-			clr(c.c2.t(i).color), clr(c.c3.t(i).color))
+	for i := 0; i < 3; i++ {
+		fmt.Printf("        | %s %s %s |\n", clr(c.d[i][0]),
+			clr(c.d[i][1]), clr(c.d[i][2]))
 	}
 	fmt.Println("        ---------")
-}
-
-func (c *cube) fillColor() {
-	clr := []int{0, 1, 2, 3}
-
-	for i := 0; i <= 3; i++ {
-		c.r1.r(i*3).color, c.r1.r(i*3+1).color, c.r1.r(i*3+2).color = clr[i], clr[i], clr[i]
-		c.r2.r(i*3).color, c.r2.r(i*3+1).color, c.r2.r(i*3+2).color = clr[i], clr[i], clr[i]
-		c.r3.r(i*3).color, c.r3.r(i*3+1).color, c.r3.r(i*3+2).color = clr[i], clr[i], clr[i]
-	}
-
-	c.c1.t(1).color, c.c1.t(2).color, c.c1.t(3).color = 4, 4, 4
-	c.c2.t(1).color, c.c2.t(2).color, c.c2.t(3).color = 4, 4, 4
-	c.c3.t(1).color, c.c3.t(2).color, c.c3.t(3).color = 4, 4, 4
-
-	c.c1.t(9).color, c.c1.t(8).color, c.c1.t(7).color = 5, 5, 5
-	c.c2.t(9).color, c.c2.t(8).color, c.c2.t(7).color = 5, 5, 5
-	c.c3.t(9).color, c.c3.t(8).color, c.c3.t(7).color = 5, 5, 5
-}
-
-func initRow() (r1 *sq) {
-	var next, prev, first *sq
-
-	prev = new(sq)
-	first = prev
-
-	for i := 0; i < 11; i++ {
-		next = new(sq)
-		prev.right, prev = next, next
-	}
-	next.right = first
-	return first
-}
-
-func initRows() (r1, r2, r3 *sq) {
-	return initRow(), initRow(), initRow()
-}
-
-func initCols(r1, r2, r3 *sq) (c1, c2, c3 *sq) {
-	c1, c2, c3 = r1, r1.r(1), r1.r(2)
-	return c1, c2, c3
-}
-
-func createLink(c1, c2 *sq) {
-	tmp := c1
-	for i := 0; i < 3; i++ {
-		next := new(sq)
-		tmp.top = next
-		tmp = next
-	}
-	tmp.top = c2
-}
-
-func linkRowsCols(r1, r2, r3, c1, c2, c3 *sq) {
-	tmp1, tmp2, tmp3 := r1, r2, r3
-
-	for i := 0; i < 3; i++ {
-		tmp3.top = tmp2
-		tmp2.top = tmp1
-		tmp1, tmp2, tmp3 = tmp1.r(1), tmp2.r(1), tmp3.r(1)
-	}
-	tmp1, tmp2, tmp3 = tmp1.r(3), tmp2.r(3), tmp3.r(3)
-
-	for i := 0; i < 3; i++ {
-		tmp1.top = tmp2
-		tmp2.top = tmp3
-		tmp1, tmp2, tmp3 = tmp1.r(1), tmp2.r(1), tmp3.r(1)
-	}
-
-	createLink(c1, c1.r(8))
-	createLink(c2, c2.r(6))
-	createLink(c3, c3.r(4))
-
-	b1, b2, b3 := r3.r(6), r3.r(7), r3.r(8)
-	createLink(b1, b1.r(8))
-	createLink(b2, b2.r(6))
-	createLink(b3, b3.r(4))
-}
-
-func initCube() (cb *cube) {
-	r1, r2, r3 := initRows()
-	c1, c2, c3 := initCols(r1, r2, r3)
-	linkRowsCols(r1, r2, r3, c1, c2, c3)
-	return &cube{r1, r2, r3, c1, c2, c3}
 }
 
 func main() {
-	c1 := initCube()
-	c1.fillColor()
-	c1.printCube()
+	var c cube
+	c.init()
+	c.printCube()
 }
